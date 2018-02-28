f25548054c895c53a10d9bd20770b24bcad8c178
mewa <marcin.k.chmiel@gmail.com>
2018-02-28 02:33:55 +0100

    Migrate to `use-package.el`

--- init.el
+++ init.el
@@ -1,77 +1,121 @@
+;; Setup Package.el
+(require 'package)
+(setq
+ package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                    ("org" . "http://orgmode.org/elpa/")
+                    ("melpa" . "http://melpa.org/packages/")
+                    ("melpa-stable" . "http://stable.melpa.org/packages/"))
+ package-archive-priorities '(("melpa-stable" . 1)))
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(setq package-list '(company company-flx monokai-theme haskell-mode company-ghc rainbow-mode markdown-mode markdown-preview-mode rainbow-delimiters))
+;; Setup use-package.er
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
 
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (add-to-list
-   'package-archives
-   '("melpa" . "https://melpa.org/packages/")
-   t)
-  (package-initialize))
+;; Window config
+(use-package switch-window
+  :config
+  (setq switch-window-shortcut-style 'qwerty)
+  :bind ("C-x o" . switch-window))
 
-(unless package-archive-contents
-  (package-refresh-contents))
+;; Ido setup
+(use-package ido
+  :init
+  (ido-mode t))
 
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
+;; Rainbow delimiters setup
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-idle-delay 0.5)
- '(custom-safe-themes
-   (quote
-    ("3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" default)))
- '(package-selected-packages
-   (quote
-    (slime-company slime yaml-mode company-ghc jsx-mode flx-isearch monokai-theme company-flx org company))))
+;; company-mode setup
+(use-package company
+  :custom
+  (company-idle-delay 0.5)
+  :config
+  (defun mewa/push-company-backend (backend)
+    "Add company BACKEND as a buffer-local"
+    (make-local-variable 'company-backends)
+    (push backend company-backends))
+  (global-company-mode t))
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(use-package company-flx
+  :after
+  (company)
+  :config
+  (company-flx-mode t))
 
-(add-hook 'after-init-hook 'global-company-mode)
-(company-flx-mode 1)
+;; Haskell setup
+(use-package haskell-mode
+  :config
+  (interactive-haskell-mode t))
 
-;; Haskell company-mode setup
-(add-hook 'haskell-mode-hook 'ghc-comp-init)
-(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-(add-hook 'company-mode-hook
-	  (lambda () (add-to-list 'company-backends 'company-ghc)))
-(setq haskell-process-type 'stack-ghci)
+(use-package intero
+  :after (haskell company)
+  :hook (haskell-mode . intero-mode))
 
 ;; Lisp setup
-(setq inferior-lisp-program "sbcl")
-(setq slime-contribs '(slime-fancy))
-(slime-setup '(slime-fancy slime-company))
+(use-package slime
+  :config
+  (setq inferior-lisp-program "sbcl")
+  (setq slime-contribs '(slime-fancy)))
 
-(load-theme 'monokai)
+(use-package slime-company
+  :after (slime))
 
+;; Elm setup
+(use-package elm-mode
+  :hook
+  (elm-mode . (lambda () (mewa/push-company-backend 'company-elm))))
+
+;; Go setup
+(use-package go-mode)
+
+(use-package company-go
+  :after (go-mode company)
+  :hook
+  (go-mode . (lambda () (mewa/push-company-backend 'company-go))))
+
+;; Set Monokai theme
+(use-package monokai-theme
+  :ensure t
+  :config
+  (load-theme 'monokai t))
+
+;; YAML setup
+(use-package yaml-mode)
+
+;; Rust setup
+(use-package rust-mode)
+
+;; Front-end
+(use-package web-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode)))
+
+;; Indent with spaces
 (setq-default indent-tabs-mode nil)
 
-;; set up line numbering
+;; Setup line numbering
 (setq linum-format "%d ")
 (global-linum-mode 1)
-(global-subword-mode 1)
+
+;; Disable menu bar
 (menu-bar-mode 0)
 
+;; Life's too short
 (fset 'yes-or-no-p 'y-or-n-p)
 
+;; Setup parenthesis
 (show-paren-mode 1)
 (electric-pair-mode 1)
-(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 
+;; Enable subword-mode globally
+(global-subword-mode t)
+
+;; Switch between header/implementation
 (global-set-key (kbd "C-c f") 'ff-find-other-file)
 
-(add-hook 'org-mode-hook
-	  (lambda () (require 'ox-md)))
+(provide 'init)
+;;; init.el ends here
